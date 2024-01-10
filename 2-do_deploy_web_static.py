74f71f3d696f9b94ff3f92ac38f97d4e64b46442
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-01-10 12:15:31 -0800

    fix .2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,8 @@ from fabric.api import *
 import os
 
 env.hosts = ['34.204.101.226', '34.232.76.130']
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/id_rsa"
+# env.user = "ubuntu"
+# env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
