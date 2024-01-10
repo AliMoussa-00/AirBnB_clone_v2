ac4a387899258e2c69a33d117f3bc6b01515265e
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-01-10 12:19:10 -0800

    .2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,8 @@ from fabric.api import *
 import os
 
 env.hosts = ['34.204.101.226', '34.232.76.130']
-# env.user = "ubuntu"
-# env.key_filename = "~/.ssh/id_rsa"
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
@@ -15,7 +15,6 @@ def do_deploy(archive_path):
     try:
 
         if not os.path.exists(archive_path):
-            print("file does not exists")
             return False
 
         archive_arr = archive_path.split('/')
