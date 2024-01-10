9a010b55449190b1e409a9385acf4b4d77d390c5
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-01-10 10:29:00 -0800

    FIX .0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,6 +23,6 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 # changing the ownership
 chown -hR ubuntu:ubuntu /data
 
-sed -i '16i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 service nginx restart
